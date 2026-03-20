b0a43894905458b2b74b7d90e62cd657347641cd
João Victor <joaovictorpinheiro1510@gmail.com>
2026-03-20 12:00:32 -0300

    feat(nvim/lua/config/keymaps): add keys to toggle terminal properly

--- src/.config/nvim/lua/config/keymaps.lua
+++ src/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Terminal
+map({ "n" }, "<C-;>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Open Terminal (Root Dir)" })
+
+map({ "t" }, "<C-;>", "<cmd>close<cr>", { desc = "Hide Terminal (Root Dir)" })
+
+-- Comments
+map("n", "<C-/>", "<esc><cmd>normal gcc<cr>", { desc = "Toggle Comment" })
