8de319167d85c72116fbb6f8ac6030df3192cc87
João Victor <joaovictorpinheiro1510@gmail.com>
2026-03-20 12:30:41 -0300

    fix(nvim): fix tab config to insert 4 spaces

--- src/.config/nvim/lua/config/options.lua
+++ src/.config/nvim/lua/config/options.lua
@@ -3,11 +3,12 @@ vim.g.lazyvim_prettier_needs_config = false
 vim.opt.relativenumber = false
 vim.opt.laststatus = 0
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smarttab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 
 vim.opt.spell = true
 vim.opt.spelllang = { "en", "pt_br" }
