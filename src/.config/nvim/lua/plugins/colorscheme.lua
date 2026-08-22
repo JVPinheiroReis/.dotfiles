06c82f7d7c33fa3c26a877c8e18d5a79e6d7a662
João Victor <joaovictorpinheiro1510@gmail.com>
2026-08-22 11:26:06 -0300

    feat(clang-format): add breaks before while, else, and catch

--- src/.config/nvim/lua/plugins/colorscheme.lua
+++ src/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,25 +3,12 @@ return {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        opts = {},
     },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     {
-        "zenbones-theme/zenbones.nvim",
-        dependencies = "rktjmp/lush.nvim",
-        lazy = false,
-        priority = 1000,
-    },
-    {
-        "jackplus-xyz/binary.nvim",
-    },
-    {
-        "y9san9/y9nika.nvim",
-    },
-    {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "y9nika",
+            colorscheme = "catppuccin",
         },
     },
 }
