3621f7eab07a327adc71673a1bfb5c67ff9f9ec9
João Victor <joaovictorpinheiro1510@gmail.com>
2026-08-26 09:10:22 -0300

    fix(nvim): change catpuccin to catpuccin-nvim

--- src/.config/nvim/lua/plugins/colorscheme.lua
+++ src/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,11 +4,22 @@ return {
         lazy = false,
         priority = 1000,
     },
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            flavour = "auto",
+            background = {
+                light = "latte",
+                dark = "mocha",
+            },
+        },
+    },
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "catppuccin",
+            colorscheme = "catppuccin-nvim",
         },
     },
 }
